9ee73d6f94ddceca4e215e43f6865fa2d051ee29
ittna <antti.maattanen@ecraft.com>
2020-03-09 14:31:30 +0200

    Week4

--- helloworld.js
+++ helloworld.js
@@ -3,10 +3,10 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 8080;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+const server = http.createServer((request, response) => {
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  response.end('Hello World!');
 });
 
 server.listen(port, hostname, () => {
